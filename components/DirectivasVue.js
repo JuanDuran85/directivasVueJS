fd47eea1bb03e53f36769f6efeb6809209f42fbd
JuanDuran85 <duran85juan@gmail.com>
2020-01-03 20:43:21 -0300

    uso de directiva v-bind

--- components/DirectivasVue.js
+++ components/DirectivasVue.js
@@ -2,11 +2,17 @@ Vue.component('directivas-vue', {
     template: `
         <div>
             <p v-text="texto"></p>
+            <a v-bind:href="link.href" :title="link.title" v-text="link.text"></a>
         </div>
     `,
     data() {
         return {
-            texto: "Uso de directiva v-text en VueJS"
+            texto: "Uso de directiva v-text en VueJS",
+            link: {
+                text: 'Vue.JS',
+                href: 'https://vuejs.org/',
+                title: 'Página web oficial de VueJS - uso de v-bind'
+            }
         }
     }
 });
